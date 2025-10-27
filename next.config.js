ab86368db2d80b9937bc029086257b67e51b13f5
Jirenn77 <jnjaso12@gmail.com>
2025-10-27 12:16:23 +0800

    Fix merge conflict in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Add if you need static export
   // output: 'export',
 }
 
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add if you need static export
-  // output: 'export',
-}
-
->>>>>>> 3a071ae (save local changes)
 module.exports = nextConfig
